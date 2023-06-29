33af2ae107c4644b7319c38189b373dbf54fa5e5
fxzz <fxzzy3@gmail.com>
2023-06-30 01:36:53 +0900

    #16 - 게시판 뷰 기본 틀 작성

--- src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
@@ -19,7 +19,7 @@ class ArticleControllerTest {
     private MockMvc mvc;
 
 
-    @Disabled("구현 중")
+
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상호출")
     @Test
     public void 게시글_리스트_정상_호출() throws Exception {
@@ -28,8 +28,8 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("article/index"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
 
