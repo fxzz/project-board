718fbc39d0a7c0989aa8bc463f070aebdbf72064
fxzz <fxzzy3@gmail.com>
2023-06-30 17:29:26 +0900

    #16 - 테스트 코드 업데이트
    테스트 코드를 더 정교하게 수정하였다

--- src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
@@ -1,31 +1,35 @@
 package com.example.projectboard.controller;
 
+import com.example.projectboard.config.SecurityConfig;
 import io.micrometer.core.instrument.binder.http.HttpRequestTags;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
 
 
-
-    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상호출")
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
-    public void 게시글_리스트_정상_호출() throws Exception {
-        //Given
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
 
-        //When & Then
+        // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
@@ -33,43 +37,45 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
-    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
-    public void 게시글_상시_페이지_정상_호출() throws Exception {
-        //Given
+    public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // Given
 
-        //When & Then
+        // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(view().name("article/detail"))
-
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
     }
 
     @Disabled("구현 중")
-    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상호출")
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
-    public void 게시글_검색_전용_페이지_정상_호출() throws Exception {
-        //Given
+    public void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        // Given
 
-        //When & Then
+        // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles/search"));
     }
 
     @Disabled("구현 중")
-    @DisplayName("[view][GET] 게시글 헤시태그 검색 페이지 - 정상호출")
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
     @Test
-    public void 게시글_헤시태그_검색_페이지_정상_호출() throws Exception {
-        //Given
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+        // Given
 
-        //When & Then
+        // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles/search-hashtag"));
     }
+
 
 }
