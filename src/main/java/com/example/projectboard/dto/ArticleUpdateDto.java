a2912b5a55ae98fdc0a916c11ff82db01be7c628
fxzz <fxzzy3@gmail.com>
2023-07-01 16:40:27 +0900

    #18 - 로직에 사용될 DTO를 추가

---
+++ src/main/java/com/example/projectboard/dto/ArticleUpdateDto.java
@@ -0,0 +1,16 @@
+package com.example.projectboard.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record ArticleUpdateDto(
+        String title,
+        String content,
+        String hashtag
+
+) implements Serializable {
+
+    public static ArticleUpdateDto of(String title, String content, String hashtag) {
+    return new ArticleUpdateDto(title, content, hashtag);
+    }
+}
